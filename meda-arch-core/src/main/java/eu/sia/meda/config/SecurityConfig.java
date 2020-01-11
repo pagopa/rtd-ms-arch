cacf6c6f14a71705939c089bf6f9217e3b63e15a
Dario Pelliccioli <dario.pelliccioli@everis.com>
2020-01-11 03:42:20 +0100

    fic security

--- meda-arch-core/src/main/java/eu/sia/meda/config/SecurityConfig.java
+++ meda-arch-core/src/main/java/eu/sia/meda/config/SecurityConfig.java
@@ -3,20 +3,22 @@ package eu.sia.meda.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.AuthorizedUrl;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.
-                csrf().disable()
-                .cors()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/actuator/(health|info)").permitAll()
-                .antMatchers("/public/*").permitAll()
-                .anyRequest().authenticated();
-    }
+	
+	/**
+	 * Configure.
+	 *
+	 * @param http the http
+	 * @throws Exception the exception
+	 */
+	protected void configure(HttpSecurity http) throws Exception {
+		((HttpSecurity) ((HttpSecurity) ((HttpSecurity) http.csrf().disable()).cors().and()).sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()).headers().cacheControl();
+		((AuthorizedUrl) http.authorizeRequests().anyRequest()).permitAll();
+	}
 
 }
