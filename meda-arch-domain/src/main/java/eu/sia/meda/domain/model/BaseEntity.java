6b254344cece80bb87624f66ac295b51c3198196
claromeo <claudia.romeo@everis.com>
2020-01-20 16:09:46 +0100

    add BaseEntity and add dependency jpa

---
+++ meda-arch-domain/src/main/java/eu/sia/meda/domain/model/BaseEntity.java
@@ -0,0 +1,31 @@
+package eu.sia.meda.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public abstract class BaseEntity implements Serializable {
+
+    @NotNull
+    @Column(name = "INSERT_DATE")
+    private LocalDateTime insertDate;
+
+    @Column(name = "INSERT_USER")
+    private String insertUser;
+
+    @NotNull
+    @Column(name = "UPDATE_DATE")
+    private LocalDateTime updateDate;
+
+    @Column(name = "UPDATE_USER")
+    private String updateUser;
+
+    @Column(name = "ENABLED")
+    private boolean enabled = true;
+}
