3702c1ae540aeccafb81ef384dca1aa7d6de89af
Torre Antonio <antonio.torre@everis.com>
2020-01-22 16:13:26 +0100

    mockito mocks init in BaseTest

--- meda-arch-core/src/test/java/eu/sia/meda/BaseTest.java
+++ meda-arch-core/src/test/java/eu/sia/meda/BaseTest.java
@@ -3,10 +3,15 @@ package eu.sia.meda;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import org.junit.BeforeClass;
+import org.mockito.MockitoAnnotations;
 import org.slf4j.LoggerFactory;
 
 /** Base class to use in order to configurate unit test */
 public class BaseTest {
+
+    public BaseTest(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @BeforeClass
     public static void configTest() {
