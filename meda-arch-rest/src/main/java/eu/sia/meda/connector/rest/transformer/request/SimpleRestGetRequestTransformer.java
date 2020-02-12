8a725af9d4c8774e8951241f0d4c3416af46db9e
Torre Antonio <antonio.torre@everis.com>
2020-02-12 15:01:04 +0100

    refactor in progress (ms-reward-event TODO)

--- meda-arch-rest/src/main/java/eu/sia/meda/connector/rest/transformer/request/SimpleRestGetRequestTransformer.java
+++ meda-arch-rest/src/main/java/eu/sia/meda/connector/rest/transformer/request/SimpleRestGetRequestTransformer.java
@@ -5,6 +5,7 @@ import eu.sia.meda.connector.rest.transformer.IRestRequestTransformer;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Map;
 
 /** To handle simple cases where
@@ -23,10 +24,16 @@ public class SimpleRestGetRequestTransformer implements IRestRequestTransformer<
         RestConnectorRequest<Void> out = new RestConnectorRequest<>();
         out.setMethod(HttpMethod.GET);
         if(args.length>0) {
+            if(args[0]==null){
+                args[0] = Collections.emptyMap();
+            }
             //noinspection unchecked
             out.setParams((Map<String, String>) args[0]);
         }
         if(args.length>1) {
+            if(args[1]==null){
+                args[1] = Collections.emptyMap();
+            }
             //noinspection unchecked
             out.setQueryParams((Map<String, String>) args[1]);
         }
