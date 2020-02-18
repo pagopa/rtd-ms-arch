f423817e72460233258da53b39fabe9bee455314
Torre Antonio <antonio.torre@everis.com>
2020-02-18 10:50:20 +0100

    JPA Base Test will define the mocked property

--- meda-arch-jpa/src/test/java/eu/sia/meda/connector/jpa/BaseJpaTest.java
+++ meda-arch-jpa/src/test/java/eu/sia/meda/connector/jpa/BaseJpaTest.java
@@ -4,7 +4,11 @@ import eu.sia.meda.BaseSpringTest;
 import eu.sia.meda.connector.jpa.config.ArchJPAConfigurationService;
 import eu.sia.meda.core.properties.PropertiesManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {
+        "connectors.jpaConfigurations.connection.mocked:true"
+})
 @Import({ArchJPAConfigurationService.class, PropertiesManager.class})
 public class BaseJpaTest extends BaseSpringTest {
 }
