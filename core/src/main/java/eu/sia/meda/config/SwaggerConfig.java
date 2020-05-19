6b7bb708dcbb878a5bc048161c77ceb80f5b954d
Pelliccioli Dario Consulente <cons.PelliccioliDario@sia.eu>
2020-05-19 12:43:03 +0200

    fix import

--- core/src/main/java/eu/sia/meda/config/SwaggerConfig.java
+++ core/src/main/java/eu/sia/meda/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import static springfox.documentation.schema.AlternateTypeRules.newRule;
 import java.lang.reflect.Type;
 import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,15 +18,20 @@ import org.springframework.data.domain.Pageable;
 import com.fasterxml.classmate.TypeResolver;
 import com.google.common.base.Predicates;
 
-//import eu.sia.meda.core.resource.BaseResource;
-//import eu.sia.meda.exceptions.response.MicroServiceExceptionResponse;
 import springfox.documentation.builders.AlternateTypeBuilder;
 import springfox.documentation.builders.AlternateTypePropertyBuilder;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -85,4 +92,30 @@ public class SwaggerConfig {
 	private AlternateTypePropertyBuilder property(Class<?> type, String name) {
 		return new AlternateTypePropertyBuilder().withName(name).withType(type).withCanRead(true).withCanWrite(true);
 	}
+
+	@Bean
+	public SecurityConfiguration security() {
+		return SecurityConfigurationBuilder.builder().scopeSeparator(",")
+				.additionalQueryStringParams(null)
+				.useBasicAuthenticationWithAccessCodeGrant(false).build();
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("apiKey", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth())
+				.forPaths(PathSelectors.any()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope(
+				"global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Collections.singletonList(new SecurityReference("apiKey",
+				authorizationScopes));
+	}
+
 }
